3172dfe6fbbbf2e16d2fb8ab7668bd1022c02eac
Geiza Caruline Costa <geiza_costa@hotmail.com>
2021-10-04 19:11:37 -0300

    Introdução ao uso de variáveis e operações aritméticas

---
+++ variaveis_e_aritmetica.py
@@ -0,0 +1,20 @@
+# Uso de variáveis e operações aritméticas simples
+
+#Método 1
+x = float(input("Entre com o 1º valor: "))
+y = float(input("Entre com o 2º valor: "))
+print("A soma dos dois valores digitados é ", (x + y) )
+
+#Método 2
+x = float(input("Entre com o 1º valor: "))
+y = float(input("Entre com o 2º valor: "))
+print("A soma dos valores", x, "e", y, "é", x + y)
+
+#Método 3
+x = float(input("Entre com o 1º valor: "))
+y = float(input("Entre com o 2º valor: "))
+print(f"A soma dos valores {x} e {y} é {x + y} ")
+
+#Método 4
+x = float(input("Digite um valor: "))
+print("O quadrado de", x, "é", (x*x))
